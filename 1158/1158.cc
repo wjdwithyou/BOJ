523216f4311ddbfa5f1287dae64f6ce2acc3adf8
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-10 17:15:47 +0900

    https://www.acmicpc.net/problem/1158

---
+++ 1158/1158.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#define MAX_SIZE 8192
+
+int queue[MAX_SIZE];
+
+void push(int*, int);
+int pop(int*);
+
+int main() {
+	int n, m;
+	int front, back;
+
+	front = back = 0;
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; ++i) {
+		push(&back, i);
+	}
+
+	printf("<");
+	for (int i = 1; front != back; ++i) {
+		if (i % m == 0) {
+			if (i == m) {
+				printf("%d", pop(&front));
+			}
+			else {
+				printf(", %d", pop(&front));
+			}
+		}
+		else {
+			push(&back, pop(&front));
+		}
+	}
+	printf(">");
+	return 0;
+}
+
+void push(int* back, int data) {
+	queue[(*back)++ % MAX_SIZE] = data;
+	return;
+}
+
+int pop(int* front) {
+	return queue[(*front)++ % MAX_SIZE];
+}
