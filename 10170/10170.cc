4615706fd0ff2bad48a5ba6c592ff2079e4fd08b
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-10 17:31:51 +0900

    https://www.acmicpc.net/problem/10170

---
+++ 10170/10170.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+	puts("NFC West       W   L  T");
+	puts("-----------------------");
+	puts("Seattle        13  3  0");
+	puts("San Francisco  12  4  0");
+	puts("Arizona        10  6  0");
+	puts("St. Louis      7   9  0");
+	printf("\n");
+	puts("NFC North      W   L  T");
+	puts("-----------------------");
+	puts("Green Bay      8   7  1");
+	puts("Chicago        8   8  0");
+	puts("Detroit        7   9  0");
+	puts("Minnesota      5  10  1");
+	return 0;
+}
