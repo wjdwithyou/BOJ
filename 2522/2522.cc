f7be3fd902a83d2e45ef22999c9ea4a4f2d1e189
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-16 18:18:46 +0900

    https://www.acmicpc.net/problem/2522

---
+++ 2522/2522.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int j = n - i; j > 0; --j) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; --i) {
+        for (int j = 0; j < n - i; ++j) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
