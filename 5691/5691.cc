fc424c07f8c54c5cf69d7ae773ce77aa246757dc
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-18 18:01:01 +0900

    https://www.acmicpc.net/problem/5691

---
+++ 5691/5691.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    
+    while (true) {
+        scanf("%d %d", &a, &b);
+        
+        if (a == 0 && b == 0) {
+            break;
+        }
+        
+        printf("%d\n", 2 * a - b);
+    }
+    
+    return 0;
+}
