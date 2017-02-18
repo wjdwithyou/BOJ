7b2641fdac80a994f5a7b1630eaa873e8e6b56ab
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-18 18:11:19 +0900

    https://www.acmicpc.net/problem/10599

---
+++ 10599/10599.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d;
+    
+    while (true) {
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        
+        if (a == 0 && b == 0 && c == 0 && d == 0) {
+            break;
+        }
+        
+        printf("%d %d\n", c - b, d - a);
+    }
+    
+    return 0;
+}
