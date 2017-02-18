9b5152906a998bdae554abe8b3ad4b9cc7b78fc2
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-18 12:12:56 +0900

    https://www.acmicpc.net/problem/2445

---
+++ 2445/2445.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            printf("*");
+        }
+        for (int j = 0; j < 2 * (n - i); ++j) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < n - i; ++j) {
+            printf("*");
+        }
+        for (int j = 0; j < 2 * i; ++j) {
+            printf(" ");
+        }
+        for (int j = 0; j < n - i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
