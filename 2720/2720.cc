745ae7a6bafaee1e05b59faecbdb406ae48ac00a
TasteWiki <administrator@mail.tastewiki.xyz>
2017-02-22 17:39:09 +0900

    https://www.acmicpc.net/problem/2720

---
+++ 2720/2720.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int t, m, q, d, n, p;
+    
+    scanf("%d", &t);
+    
+    for (int i = 0; i < t; ++i) {
+        scanf("%d", &m);
+        
+        q = m / 25;
+        m %= 25;
+        
+        d = m / 10;
+        m %= 10;
+        
+        n = m / 5;
+        p = m % 5;
+        
+        printf("%d %d %d %d\n", q, d, n, p);
+    }
+    
+    return 0;
+}
