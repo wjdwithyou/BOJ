7e1c3a8c1e99287a6b695008e2041f6db9686513
TasteWiki <administrator@mail.tastewiki.xyz>
2017-03-08 14:41:00 +0900

    https://www.acmicpc.net/problem/11727

---
+++ 11727/11727.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int arr[1024];
+
+int main() {
+    int n;
+    
+    arr[1] = 1;
+    arr[2] = 3;
+    
+    scanf("%d", &n);
+    
+    for (int i = 3; i <= n; ++i) {
+        arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+    }
+    
+    printf("%d", arr[n]);
+    return 0;
+}
