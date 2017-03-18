0116de58bd0df78e4284063483371d5fecf508e8
TasteWiki <administrator@mail.tastewiki.xyz>
2017-03-18 12:20:27 +0900

    https://www.acmicpc.net/problem/10815

---
+++ 10815/10815.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define MAX 10000000
+
+bool arr[2 * MAX + 1];
+
+int main() {
+    int n, m, t;
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &t);
+        ++arr[t + MAX];
+    }
+    
+    scanf("%d", &m);
+    
+    for (int i = 0; i < m; ++i) {
+        scanf("%d", &t);
+        printf("%d ", arr[t + MAX]);
+    }
+    
+    return 0;
+}
