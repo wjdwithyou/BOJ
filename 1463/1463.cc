4ad0377465413d7997476789767ab528732bf52d
TasteWiki <administrator@mail.tastewiki.xyz>
2017-06-26 23:45:12 +0900

    https://www.acmicpc.net/problem/1463

--- 1463/1463.cc
+++ 1463/1463.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 
-#pragma warning (disable:4996)
-
 int arr[1048576];
 int queue[1048576];
 
@@ -12,28 +10,32 @@ int main() {
 	int front = 0, back = 0;
     
 	scanf("%d", &n);
-	queue[back++] = n;
-    arr[n] = 0;
     
-	printf("%d", bfs(&front, &back));
-	return 0;
+	queue[back++] = n;
+	arr[n] = 0;
+    
+	return !printf("%d", (n == 1) ? 0 : bfs(&front, &back));
 }
 
 int bfs(int* front, int* back) {
-    int n = queue[(*front)++];
+	int o_back = *back;
     
-    if (n % 3 == 0 && arr[n / 3] == 0) {
-        queue[(*back)++] = n / 3;
-        arr[n / 3] = arr[n] + 1;
-    }
-    if ((n & 1) == 0 && arr[n >> 1] == 0) {
-        queue[(*back)++] = n >> 1;
-        arr[n >> 1] = arr[n] + 1;
-    }
-    if (arr[n - 1] == 0) {
-        queue[(*back)++] = n - 1;
-        arr[n - 1] = arr[n] + 1;
-    }
+	while (*front < o_back) {
+		int n = queue[(*front)++];
+        
+		if (n % 3 == 0 && arr[n / 3] == 0) {
+			queue[(*back)++] = n / 3;
+			arr[n / 3] = arr[n] + 1;
+		}
+		if ((n & 1) == 0 && arr[n >> 1] == 0) {
+			queue[(*back)++] = n >> 1;
+			arr[n >> 1] = arr[n] + 1;
+		}
+		if (arr[n - 1] == 0) {
+			queue[(*back)++] = n - 1;
+			arr[n - 1] = arr[n] + 1;
+		}
+	}
     
-    return (arr[1] != 0) ? arr[1] : bfs(front, back);
+	return (arr[1] != 0) ? arr[1] : bfs(front, back);
 }
