60ceb7a156989768bcbc4c9563dd2a2a31e6af6a
TasteWiki <administrator@mail.tastewiki.xyz>
2017-07-29 16:53:29 +0900

    https://www.acmicpc.net/problem/14645

---
+++ 14645/14645.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int n, k, a, b;
+    
+    scanf("%d %d", &n, &k);
+    
+    for (int i = 0; i < n; ++i) {
+        scanf("%d %d", &a, &b);
+    }
+    
+    return !printf("비와이");
+}
